1c171a3764f429ada69861903ed668f093b073f4
Vicky Lee <ytleevicky@gmail.com>
2023-02-09 00:52:45 -0500

    Rename file

---
+++ src/components/styles/Navbar.styles.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Nav = styled.div`
+  background-color: #fff;
+  color: #000000;
+  width: 100%;
+  height: 55px;
+  padding-left: 20px;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+
+  display: flex;
+  align-items: center;
+  font-size: 2em;
+  font-weight: bold;
+  line-height: 1.4;
+`;
