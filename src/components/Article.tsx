854e01c2a0232d8fee43a5ac5830e4a5271ccb81
Vicky Lee <ytleevicky@gmail.com>
2023-02-11 16:24:10 -0500

    Scrape image from URL metadata

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -12,8 +12,6 @@ import {
   ImageContainer,
 } from "./styles/Article.styles";
 
-import tempImage from "../images/articleImage.png";
-
 type ArticleProps = {
   articleID: number;
 };
@@ -27,21 +25,48 @@ export const Article: React.FC<ArticleProps> = (props: ArticleProps) => {
   }>();
 
   const [loading, setLoading] = useState(true);
+  const [image, setImage] = useState("");
+
+  const scrapeImage = async (url: RequestInfo | URL) => {
+    const response = await fetch(url);
+    const text = await response.text();
+    const parser = new DOMParser();
+    var imgVal = "";
+
+    const htmlDoc = parser.parseFromString(text, "text/html");
+    const image = htmlDoc
+      .querySelector('meta[property="og:image"]')
+      ?.getAttribute("content");
+
+    if (image) {
+      // Have image metadata from URL
+      imgVal = String(image);
+      setImage(imgVal);
+    } else {
+      // No image metadata is found -> Generate a random images
+      const response = await fetch(
+        "https://picsum.photos/200/300?date=" + new Date().toLocaleString()
+      );
+      const url = response.url;
+      setImage(url);
+    }
+  };
 
   useEffect(() => {
     fetch(`https://hacker-news.firebaseio.com/v0/item/${props.articleID}.json`)
       .then((res) => res.json())
-      .then((json) => {
+      .then(async (json) => {
         setArticle(json);
         setLoading(false);
+        scrapeImage(json.url);
       });
-  });
+  }, []);
 
   return (
     <Fragment>
       <ArticleContainer>
         <ImageContainer>
-          <ArticleImage src={tempImage} />
+          <ArticleImage src={image} />
         </ImageContainer>
 
         {loading ? (
